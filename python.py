43b89fb71e108ae565be4a2190c03dcf3dcaaccb
PatrickStojek <patryk.stojek7@gmail.com>
2024-02-19 22:34:02 +0100

    feat: add decimal to binary function

--- python.py
+++ python.py
@@ -1,1 +1,9 @@
-print("Test!")
+def decimal_to_binary(decimal_num) {
+    if decimal_num == 0:
+        return '0'
+    binary_num - ''
+    while decimal_num > 0:
+        binary_num = str(decimal_num % 2) + binary_num
+        decimal_num = decimal_num // 2
+    return binary_num
+}
