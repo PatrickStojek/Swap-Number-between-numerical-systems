ca8f6386d06e44873bba78f85a251724ba65e48d
PatrickStojek <patryk.stojek7@gmail.com>
2024-02-25 22:58:46 +0100

    finish binary_to decimal function

--- script.js
+++ script.js
@@ -10,4 +10,29 @@ function decimal_to_binary(decimal_num) {
     return binary_num
 }
 
+    function binary_to_decimal(binary_num) {
+        let binary_str;
+        
+        if (typeof binary_num === 'number') {
+            binary_str = String(binary_num);
+        } else if (typeof binary_num === 'string') {
+            binary_str = binary_num;
+        } else {
+            throw new TypeError("Input must be an integer or a string representing a binary number");
+        }
+        
+        let decimal_num = 0;
+        let power = binary_str.length - 1;
+        
+        for (let digit of binary_str) {
+            if (!['0', '1'].includes(digit)) {
+                throw new TypeError("Binary number must contain only '0' and '1'");
+            }
+            decimal_num += parseInt(digit) * Math.pow(2, power);
+            power -= 1;
+        }
+        
+        return decimal_num;
+    }
+
 console.log(decimal_to_binary(12))
