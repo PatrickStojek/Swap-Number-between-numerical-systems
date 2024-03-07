1742165cb84d87f1a53e056e56546c97224733a5
PatrickStojek <patryk.stojek7@gmail.com>
2024-03-07 22:47:53 +0100

    fix few indentation errors in python file

--- python.py
+++ python.py
@@ -10,7 +10,7 @@ def decimal_to_binary(decimal_num) :
 
 """ print(decimal_to_binary(123)) """
 
-def binary_to_decimal(binary_num);
+def binary_to_decimal(binary_num):
     if isinstance(binary_num, int) :
         binary_str = str(binary_num)
     elif isinstance(binary_num, str):
@@ -21,15 +21,15 @@ def binary_to_decimal(binary_num);
     decimal_num = 0
     power = len(binary_str) - 1
     for digit in binary_str:
-        if digit in '01':
-            rasie TypeError("Binary number must contain only '0' and '1'")
+        if digit not in '01':
+            raise TypeError("Binary number must contain only '0' and '1'")
         decimal_num += int(digit) * (2**power)
         power -= 1
     return decimal_num     
 print(binary_to_decimal(1100))
 
 
-def decimal_to_any_base(decimal_num, base) :
+def daecimal_to_any_bse(decimal_num, base) :
     if base < 2 or base > 33:
         raise ValueError("Base must be between 2 and 33")
     if(decimal_num) == 0:
@@ -37,27 +37,28 @@ def decimal_to_any_base(decimal_num, base) :
     any_base_num = ''
     while decimal_num > 0:
         remainder = decimal_num % base
-    if remainder < 10:
-        any_base_num = str(remainder) + any_base_num
-    else:
-        any_base_num = chr(remainder + 55) + any_base_num
-    decima_num = decimal_num // base
+        if remainder < 10:
+            any_base_num = str(remainder) + any_base_num
+        else:
+            any_base_num = chr(remainder + 55) + any_base_num
+        decima_num = decimal_num // base
     return any_base_num
 
 
 def any_base_to_decimal(any_base_number, base) :
         decimal_num = 0
-        power = len(num) - 1
-            for digit in any_base_number :
-                if '0' <= digit <= '9':
-                    decimal_num += int(digit) * (base ** power)
-                else:
-                    decimal_num += (ord(digit) - 55) * (base ** power)
-                power -= 1
-            return decimal_num
+        power = len(any_base_number) - 1
+        for digit in any_base_number :
+            if '0' <= digit <= '9':
+                decimal_num += int(digit) * (base ** power)
+            else:
+                decimal_num += (ord(digit) - 55) * (base ** power)
+            power -= 1
+        return decimal_num
 
 def any_base_to_any_base(number1, base1, base2) :
     decimal_number = any_base_to_decimal(number1, base1)
     return decimal_to_any_base(decimal_number, base2)
 
 
+any_base_to_any_base('123',10,16)
