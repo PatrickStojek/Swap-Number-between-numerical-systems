99ed1fa9acb51b6cf3b01a1f72e8c4ad19632905
PatrickStojek <patryk.stojek7@gmail.com>
2024-03-08 23:52:41 +0100

    add_in_ base function

--- operations.py
+++ operations.py
@@ -9,3 +9,21 @@ def value_to_digit(value):
         return chr(value + ord('0'))
     else:
         return chr(value - 10 + ord('A'))
+
+def add_in_base(number1, number2, base):
+    result = ''
+    carry = 0
+
+    # Wyrównaj długości obu liczb
+    number1 = number1.rjust(max(len(number1), len(number2)), '0')
+    number2 = number2.rjust(max(len(number1), len(number2)), '0')
+
+    # Dodawanie od końca
+    for i in range(len(number1) - 1, -1, -1):
+        sum = digit_to_value(number1[i]) + digit_to_value(number2[i]) + carry
+        carry = sum // base
+        result = value_to_digit(sum % base) + result
+
+    if carry > 0:
+        result = value_to_digit(carry) + result
+    return result
