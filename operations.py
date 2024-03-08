b408c56f83c0583c4438da5e2199eeea810fbce9
PatrickStojek <patryk.stojek7@gmail.com>
2024-03-08 23:41:44 +0100

    add value_to_digit function

--- operations.py
+++ operations.py
@@ -3,3 +3,9 @@ def digit_to_value(digit):
         return ord(digit) - ord('0')
     else:
         return ord(digit.upper()) - ord('A') + 10
+
+def value_to_digit(value):
+    if value < 10:
+        return chr(value + ord('0'))
+    else:
+        return chr(value - 10 + ord('A'))
